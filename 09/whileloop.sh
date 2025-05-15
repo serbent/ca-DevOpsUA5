27a95a887a01428f6b9663a28e12e585660fb4a8
Serbent <andrius.solovej@gmail.com>
2025-05-15 19:17:10 +0300

    Push some scripts

--- 09/whileloop.sh
+++ 09/whileloop.sh
@@ -6,11 +6,21 @@ while [ '1' == '1' ]
 do
     echo "To the infinity and beyond!!!!"
     sleep 2
-
+((COUNTER++))
     if [ $COUNTER -ge 3 ]; then
         break;
         echo $COUNTER
     fi
 
-((COUNTER++))
+
 done
+
+# if [ $COUNTER ]; then 
+#     echo "The variable is  so it must be TRUE"
+
+#     while [ '1' == '1' ]
+#     do 
+#         echo "The variable is still true"
+#         sleep 2
+#     done
+# fi
