a84b729581704eec77272dea4bbb4ee765ae4e2a
Serbent <andrius.solovej@gmail.com>
2025-05-19 19:48:50 +0300

    Lib.sh

--- 10/lib.sh
+++ 10/lib.sh
@@ -1,3 +1,17 @@
+# -----------------------------------------------------------------------------
+# Function: logtofile
+# Logs a message to a file named after the current date.
+#
+# Parameters:
+#   $1 - Log level (1=INFO, 2=WARNING, 3=ERROR)
+#   $2 - Message to log
+#
+# Behavior:
+#   - Prepends each log entry with a timestamp and log level.
+#   - Appends the log entry to a file named "$DATE.log".
+#   - Uses the global variable $DATE for the log file name (must be set elsewhere).
+# -----------------------------------------------------------------------------
+
 function logtofile() {
     case $1 in
     1)
