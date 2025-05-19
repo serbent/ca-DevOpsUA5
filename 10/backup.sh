31d1b1ad7601675282e392da2068b1c6273027d1
Serbent <andrius.solovej@gmail.com>
2025-05-19 19:46:42 +0300

    Arguments

--- 10/backup.sh
+++ 10/backup.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
-
+source lib.sh
 DATE=`date '+%Y-%m-%d'`
 # set -x 
 set -e
 
 echo "Hello World!"
 
-function logtofile() {
-    case $1 in
-    1)
-        local INFOLEVEL='[INFO]'
-        ;;
-    2) 
-        local INFOLEVEL='[WARNING]'
-        ;;
-    3) 
-        local INFOLEVEL='[ERROR]'
-        ;;
-    esac
 
-    echo "[`date '+%d-%m-%Y %H:%M:%S'`]" $INFOLEVEL" "$2 >> $DATE.log
-}
 
 function backup() {
     pushd /home
