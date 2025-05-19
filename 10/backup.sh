209c9c48a95219e56cef8eee9e73881fd2bfada3
Serbent <andrius.solovej@gmail.com>
2025-05-19 19:35:19 +0300

    Backup script

--- 10/backup.sh
+++ 10/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=`date '+%Y-%m-%d'`
-set -x 
+# set -x 
 set -e
 
 echo "Hello World!"
@@ -29,8 +29,14 @@ function backup() {
     popd
     for DIR in $LIST
     do
-        pwd
-        tar -cf $DIR-$DATE.tar /home/$DIR
+        # Check if the directory we are about to backup exists
+        if [ -d /home/$DIR ]; then
+            tar -cf $DIR-$DATE.tar /home/$DIR
+        else 
+            logtofile "3" "Directory does not exist"
+            return 1
+        fi
+        # Check if the archive file was created
         if [ -f $DIR-$DATE.tar ]; then 
             gzip -9 $DIR-$DATE.tar
         else 
