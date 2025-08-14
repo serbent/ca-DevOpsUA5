2d8ccc685a5456774fefe335f1f8e697a8846be4
Serbent <andrius.solovej@gmail.com>
2025-08-14 18:42:17 +0300

    48.

---
+++ 48/oidc/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "ca-devops-ua5-terraform-state"
+    key            = "terraform/48/terraform.tfstate"
+    region         = "eu-central-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
